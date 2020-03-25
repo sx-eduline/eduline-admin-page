bbf88dab5faf7550b737ac49037f545a44656127
xiaosun <syh@sunyonghong.com>
2020-03-25 09:27:00 +0800

    添加富文本编辑器组件

---
+++ src/libs/pageform/items/Editor.php
@@ -0,0 +1,10 @@
+<?php
+declare (strict_types = 1);
+namespace eduline\admin\libs\pageform\items;
+
+use eduline\admin\libs\pageform\items\interfaces\ArgsInterface;
+
+class Editor extends ArgsInterface
+{
+    protected $type = 'editor';
+}
